4744003fe39d8050363602322090246f8c24a44f
Juan Andres Caycedo S <ja.caycedo@uniandes.edu.co>
2021-04-07 19:14:52 -0500

    primero

---
+++ app-back/front/src/components/Jobs.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Job from "./Job";
+
+export default class Jobs extends React.Component {
+
+  componentDidMount() {
+    const url = "/offers";
+    fetch(url)
+      .then(res => {
+        return res.json();
+      }).then(offers => {
+        this.setState({ offers })
+      })
+  }
+
+  state = {
+    "offers": []
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.offers.map((e, i) => <Job key={i} offer={e} />)}
+      </div>
+    );
+  }
+}
